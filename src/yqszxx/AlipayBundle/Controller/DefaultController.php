92fba631eb920540f98cb0c850b97973c8c3e64a
Harry Han <yqszxx@gmail.com>
2015-04-05 20:24:38 +0900

    可以正常生成付款链接了

--- src/yqszxx/AlipayBundle/Controller/DefaultController.php
+++ src/yqszxx/AlipayBundle/Controller/DefaultController.php
@@ -5,18 +5,23 @@ namespace yqszxx\AlipayBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use yqszxx\AlipayBundle\Alipay\TradeCreateByBuyer;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/alipay/debug")
+     * @Route("/alipay/debug", name="alipay_debug")
      * @Template()
      */
     public function indexAction()
     {
-        $name = new TradeCreateByBuyer($this->container);
-        dump($name);
-        return array('name' => 'yqszxx');
+        $tcb = $this->container->get('alipay_tcb');
+        $url = $tcb
+            ->setSubject('TestGood')
+            ->setPrice(0.01)
+            ->setNotifyUrl('http://www.itfls.com/a.php')
+            ->setReturnUrl('http://www.itfls.com/a.php')
+            ->setOutTradeNo('8544601198343944')
+            ->getUrl();
+        return array('url' => $url);
     }
 }
